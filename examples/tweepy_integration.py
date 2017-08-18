5dbc490b9ef94be1bdf43f2de22a98e7d0eb0557
Kenny Joseph <josephkena@gmail.com>
2017-08-18 15:49:20 -0400

    updating tweepy

--- examples/tweepy_integration.py
+++ examples/tweepy_integration.py
@@ -1,42 +1,52 @@
-from __future__ import absolute_import, print_function
-
+import io
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from twitter_dm import Tweet
 import requests
 import sys
+import gzip
 
-consumer_key = "YOUR_CONSUMER_KEY_HERE"
-consumer_secret = "YOUR_CONSUMER_SECRET_HERE"
-access_token = "YOUR_ACCESS_TOKEN_HERE"
-access_token_secret = "YOUR_ACCESS_TOKEN_SECRET_HERE"
+consumer_key = "ax8ZTDCTf1nllVyFs384lYHqE"
+consumer_secret = "GA9NXowSo6hMQHdP0Un0aZK1qgck6BS6qDcDD5QfET08XMCM6j"
+access_token = "2798401248-uTPiFgXDktgoXw40a85bzmpe106AX5kCc8zCFxd"
+access_token_secret = "Yg4w06KeqQK02ucgaVX5iqX2ld5BEAE5QeEUgWbsvByua"
 
 user_set = set()
-SAMPLE_SIZE = 100000
+SAMPLE_SIZE = 5000000
+nt = 0
+of = gzip.open(sys.argv[1],"wb")
 class BaseTweepyListener(StreamListener):
     def __init__(self):
         super(StreamListener, self).__init__()
-
+        self.nt = 0
+        
     def on_data(self, data):
-        try:
-            t = Tweet(data, do_tokenize=False)
+        if self.nt % 1000 == 0:
+            print(self.nt)
+        self.nt +=1 
+        #try:
+        #    t = Tweet(data, do_tokenize=False)
 
-            if t.lang != 'en':
-                return
-            user_set.add(t.user.id)
-        except:
-            return True
+        #    if t.lang != 'en':
+        #        return
+        #    user_set.add(t.user.id)
+        #except:
+        #    return True
 
-        if len(user_set) % 1000 == 0:
-            print(len(user_set))
-        if len(user_set) == SAMPLE_SIZE:
-            of = open("sampled_user_ids.txt","w")
-            for u in user_set:
-                of.write(str(u)+"\n")
-            of.close()
-            sys.exit(-1)
-
+        #if len(user_set) % 1000 == 0:
+        #    print(len(user_set))
+        #if len(user_set) == SAMPLE_SIZE:
+        #    of = open("sampled_user_ids.txt","w")
+        #    for u in user_set:
+        #        of.write(str(u)+"\n")
+        #    of.close()
+        #    sys.exit(-1)
+        if data:
+            try:
+                of.write((data.strip()+u"\n").encode("utf8"))
+            except:
+                pass
         return True
 
     def on_error(self, status):
@@ -47,12 +57,11 @@ if __name__ == '__main__':
     l = BaseTweepyListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-
+    
     while True:
         try:
             stream = Stream(auth, l)
-            stream.filter(track=['clinton','trump','@realDonaldTrump','@HillaryClinton',
-                                 'Hillary','#maga','#imwither','donald','#election2016'])
+            stream.filter(track=['statue','unitetheright','march on google'])
         except requests.packages.urllib3.exceptions.ProtocolError:
             pass
         except requests.packages.urllib3.exceptions.ReadTimeoutError:
