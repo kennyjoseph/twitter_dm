5426ea255ee351043c0ead764277cc55b82db027
mbenigni <matthew.benigni@gmail.com>
2021-04-21 13:42:09 -0400

    remove dependencies

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests', 'rauth', 'nltk', 'openpyxl'],
+    install_requires=['requests', 'rauth', 'nltk'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
